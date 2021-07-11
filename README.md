67197fbdddc6cb28c70cd8f2ec3d575be0184cfe
jr <jr@jirouette.com>
2021-07-11 15:33:57 +0200

    Publishing the script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Strasbourg Residants Firefox
+
+Allow Firefox users to use Strasbourg's website for [parking residents](https://residants.strasbourg.eu)
+
+You will need [TamperMonkey](https://addons.mozilla.org/fr/firefox/addon/tampermonkey/) to install this plugin. 
+
+## License
+
+This project is distribued under WTFPL
---
+++ strasbourg-residants-firefox.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Strasbourg Residants Firefox
+// @namespace    https://residants.strasbourg.eu/
+// @version      1.0
+// @description  Fix Strasbourg's shitty website for them since they don't want to
+// @author       jr (https://github.com/jirouette)
+// @match        https://residants.strasbourg.eu/*
+// @icon         https://www.google.com/s2/favicons?domain=strasbourg.eu
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.onunload = "";
+})();
